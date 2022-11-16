efa0d5ac1cf7aa3a41b4d8020878340ecd675a51
Yana Priyma <yanaprima@rambler.ru>
2022-11-16 19:22:47 +0300

    Add Task08 - дз семинар 1 - задача 08

---
+++ Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("введите число: ");
+int N  = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+while (counter <= N)
+{
+    if ( counter % 2 == 0)
+    {
+        Console.WriteLine(counter);
+    }
+    counter++;
+}
+
