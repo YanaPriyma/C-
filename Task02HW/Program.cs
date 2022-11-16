c6a21cb7ec462f162380754badc62cdf8be4cb3f
Yana Priyma <yanaprima@rambler.ru>
2022-11-16 17:48:40 +0300

    Add Task02HW - дз семинар 1 - задача 02

---
+++ Task02HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max =  {a}");   
+}
+else
+{
+    Console.WriteLine($"max =  {b}"); 
+}
