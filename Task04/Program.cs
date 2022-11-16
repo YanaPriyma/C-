9aad0c342d4eeff8ace2df2fb1f8db74cbe218ab
Yana Priyma <yanaprima@rambler.ru>
2022-11-16 19:01:01 +0300

    Add Task04 - дз семинар 1 - задача 04

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine($"max =  {a}");   
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine($"max =  {b}"); 
+}
+else
+{
+    Console.WriteLine($"max =  {c}"); 
+}
