5d9c206745cc4a059c7df5064fc36b06f6fdcebb
Yana Priyma <yanaprima@rambler.ru>
2022-11-17 22:03:08 +0300

    Add Task13 - дз семинар2

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CheckThirdDigit(int num)
+{
+    while (Math.Abs(num) >= 1000) num = num / 10;
+    int res = num % 10;
+    return Math.Abs(res);
+}
+
+if (Math.Abs(number) >= 100)
+{
+    Console.WriteLine(CheckThirdDigit(number));
+}
+else Console.WriteLine("третьей цифры нет");
