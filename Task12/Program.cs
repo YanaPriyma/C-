1872447b3093783784073db137db058d16f16223
Yana Priyma <yanaprima@rambler.ru>
2022-11-17 19:29:38 +0300

    Add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает на вход 2 числа  и проверяет кратно ли первое число второму, 
+// если не кратно -  выводит остаток от деления
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity (int number1, int number2)
+{
+    if (number1 % number2 == 0) return true;
+    else return false;
+}
+bool result = Multiplicity(num1, num2);
+Console.WriteLine(result ? "Кратно" : $"Некратно, остаток = {num1 % num2}");
