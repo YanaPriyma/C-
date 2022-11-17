8bca31e9185646614d31b29a506db3782d2b38e4
Yana Priyma <yanaprima@rambler.ru>
2022-11-17 18:15:03 +0300

    Add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// выводит случайное трехзначное число и удаляет вторую цифру 
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Рандобное число --> {number}");
+
+int RemoveSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int result = firstDigit*10 + lastDigit;
+    return result;
+}
+
+int res = RemoveSecondDigit(number);
+Console.WriteLine($"Искомое число --> {res}");
