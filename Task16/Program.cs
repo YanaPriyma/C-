aa36b3869396e5660781c9204446170322a8fe68
Yana Priyma <yanaprima@rambler.ru>
2022-11-17 20:37:20 +0300

    Add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿// на вход 2 числа и проверяется является ли одно квадратом другого
+Console.WriteLine("Введите 2 числа на проверку:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square (int num1, int num2)
+{
+    return ((num1 == num2*num2) || (num2 == num1*num1));
+}
+
+Console.WriteLine(Square(number1, number2) ? "да" : "нет");
