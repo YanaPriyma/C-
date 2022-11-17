80cdfc1e2948e12c99552808dd8462513f753aff
Yana Priyma <yanaprima@rambler.ru>
2022-11-17 20:05:51 +0300

    Add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход число и проверяет является ли кратным одновременно 7 и 23
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity (int num)
+{
+    return (num % 7 == 0 && num % 23 == 0);
+}
+
+Console.WriteLine(Multiplicity(number) ? "Да" : "Нет");
