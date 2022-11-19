489cdcf035f81f2713fc873aaf8bdb30cdfe42e3
Yana Priyma <yanaprima@rambler.ru>
2022-11-19 15:48:05 +0300

    Add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите число N:");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+string Square(int num)
+{
+    int count = 1;
+    string table = "";
+    while (count <= num)
+    {
+        table = table + $"{count} --> {count*count} \n";
+        count++;
+    }
+    return table;
+}
+
+string sqrTable = Square(N);
+Console.WriteLine(sqrTable);
