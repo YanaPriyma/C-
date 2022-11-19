af8116640fdd136feaab1b9deb4bda83b2a4e16d
Yana Priyma <yanaprima@rambler.ru>
2022-11-19 18:35:10 +0300

    Add Task19 - дз семинар 3

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int Second(int n)
+{
+    while (n >= 100) n = n / 10;
+    int second = n % 10;
+    return second;       
+}
+
+int digit2 = Second(num);
+
+bool Polindrome(int n, int m)
+{
+    if (((n / 10000) == (n % 10)) & (m ==  ((n % 100 - n % 10)/10))) return true;
+    else return false;  
+}
+
+if (num.ToString().Length == 5)
+{
+    bool result = Polindrome(num, digit2);
+    Console.WriteLine(result ? $"{num} --> да" : $"{num} --> нет");
+}
+else Console.WriteLine("Неверное число. Введите число длиной в 5 знаков");
+
