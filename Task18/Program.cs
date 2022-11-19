aff78e109abb673923bea7191e89c4ff8e14bf84
Yana Priyma <yanaprima@rambler.ru>
2022-11-19 14:49:17 +0300

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿// по заданному номеру четверти выдвет диапазон возможных данных
+
+Console.WriteLine("Укажите номер четверти");
+string quarter = Convert.ToInt32(Console.ReadLine());
+
+
+string Range (string xc)
+{
+    if (xc == "1") return "x > 0, y > 0";
+    if (xc == "2") return "x < 0, y > 0";
+    if (xc == "3") return "x < 0, y < 0";
+    if (xc == "4") return "x > 0, y < 0";
+    return "недопустимая четверть";
+}
+string range = Range(quarter);
+Console.WriteLine(range);
+
