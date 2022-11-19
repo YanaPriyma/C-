317738f93c2452065747f48c01a86c5816db9053
Yana Priyma <yanaprima@rambler.ru>
2022-11-19 20:51:07 +0300

    Add Task19 - дз семинар3 крайняя версия

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,24 +5,15 @@
 Console.WriteLine("Введите пятизначное число: ");
 int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-int Second(int n)
+bool Polindrome(int n)
 {
-    while (n >= 100) n = n / 10;
-    int second = n % 10;
-    return second;       
-}
-
-int digit2 = Second(num);
-
-bool Polindrome(int n, int m)
-{
-    if (((n / 10000) == (n % 10)) & (m ==  ((n % 100 - n % 10)/10))) return true;
+    if (((n / 10000) == (n % 10)) & (((n/1000)%10) ==  ((n % 100 - n % 10)/10))) return true;
     else return false;  
 }
 
 if (num.ToString().Length == 5)
 {
-    bool result = Polindrome(num, digit2);
+    bool result = Polindrome(num);
     Console.WriteLine(result ? $"{num} --> да" : $"{num} --> нет");
 }
 else Console.WriteLine("Неверное число. Введите число длиной в 5 знаков");
---
+++ Task21/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки А ");
+Console.WriteLine("xa: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ya: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("za: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B ");
+Console.WriteLine("xb: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("yb: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("zb: ");
+int zb = Convert.ToInt32(Console.ReadLine());
