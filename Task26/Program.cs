fcc66d8f5193e2dffc4332772eef9186768b072c
Yana Priyma <yanaprima@rambler.ru>
2022-11-23 22:57:17 +0300

    Add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿// gпринимает число и выдает количество цифр в числе
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumLength(int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        counter ++;
+    }
+    return counter;
+}
+
+int numLength = NumLength(number);
+Console.WriteLine($"{number} -> {numLength}");
