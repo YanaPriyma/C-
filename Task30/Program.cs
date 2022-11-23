697b94bef6c4395f099bc4cc69254b5e0d625284
Yana Priyma <yanaprima@rambler.ru>
2022-11-23 23:53:41 +0300

    Add Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,16 @@
+﻿// выводит массив из 8 элементов заполненный 0 и 1 в случайном порядке
+
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
+
