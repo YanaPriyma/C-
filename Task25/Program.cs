6947240d8db6bf6e1c259e7ca7ab297461b596a7
Yana Priyma <yanaprima@rambler.ru>
2022-11-24 15:11:44 +0300

    Add Task25 - дз семинар 4

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+Console.WriteLine("Введите натуральное число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Degree (int x, int y)
+{
+    int res = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        res *= x;
+    }
+    return res;
+}
+
+int degree = Degree(a, b);
+Console.WriteLine($"{a}, {b} -> {degree}");
