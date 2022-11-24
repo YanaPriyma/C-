97b4b5311da637d7407f5097049e57a6c097126a
Yana Priyma <yanaprima@rambler.ru>
2022-11-24 16:22:18 +0300

    Add Task29 - дз семинар 4

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1,10);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
