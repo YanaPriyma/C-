83f4f3cb5731bc68d8593afc2b6e063a947343da
Yana Priyma <yanaprima@rambler.ru>
2022-11-24 15:45:28 +0300

    Add Task27 - дз семинар 4

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число:");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int SumDigits (int x)
+{
+    int sum = 0;
+    while (x > 0) 
+    {
+        sum = sum + x % 10;
+        x = x / 10;
+    }
+    return sum;
+}
+
+int sumDigits = SumDigits(a);
+Console.WriteLine($"{a} -> {sumDigits}");
