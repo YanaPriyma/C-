7046fd38c40cbf54cf151e48fa26d2983b7cb8f2
Yana Priyma <yanaprima@rambler.ru>
2022-12-03 20:30:53 +0300

    Add Task47 - дз семинар 7

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, double min, double max)
+ {
+    double[,] matrix = new double[rows,columns];
+    Random rnd = new Random();
+    
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;   
+        }
+    }
+    return matrix;
+ }
+
+ void PrintMatrix (double[,] matrix)
+ {
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{Math.Round(matrix[i, j],1), 5}, ");
+            else Console.Write($"{Math.Round(matrix[i, j],1), 5}");
+        }
+        Console.WriteLine("|");
+    }
+ }
+
+Console.WriteLine("Введите число строк массива");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] array2d = CreateMatrixRndDouble(row, column, -10, 10);
+PrintMatrix(array2d);
