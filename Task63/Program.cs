d80d31c47cd9b4168d7a12778690f7c08ee61e82
Yana Priyma <yanaprima@rambler.ru>
2022-12-12 00:41:00 +0300

    Add Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Выведите натуральные числа от 1 до N
+
+Console.WriteLine("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if (num ==0) return;
+    NaturalNumbers (num -1);
+    Console.Write($"{num} ");
+}
+
+NaturalNumbers(number);
+
