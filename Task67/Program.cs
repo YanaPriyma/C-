977e4f98593e3ffaba1c2f06bf9b28183cfa0616
Yana Priyma <yanaprima@rambler.ru>
2022-12-12 23:41:34 +0300

    Add Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает цифру - выдает сумму цифр 
+Console.WriteLine("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+
+    return  num != 0 ? num % 10 + SumDigits(num / 10) : 0;
+}
+
+Console.WriteLine(SumDigits(number));
