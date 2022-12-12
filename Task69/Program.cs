b656155f9b0741fc1a54a0b9147eba983917caf1
Yana Priyma <yanaprima@rambler.ru>
2022-12-13 00:00:32 +0300

    Add Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2 числа, рекурсия возводит а в степень б
+Console.WriteLine("Введите число a:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int DegreeNumber(int number, int degree)
+{
+    if (degree == 0) return 1;
+    return DegreeNumber(number, degree - 1) * number;
+}
+
+Console.WriteLine(DegreeNumber(numA, numB));
